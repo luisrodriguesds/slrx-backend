1d5c06a46674e4d26ae1d33b5edb0d5769537497
Luis Rodrigues <luisitaloar@gmail.com>
2019-09-14 18:13:51 -0300

    Ajustes Gerais

--- app/Models/Solicitation.js
+++ app/Models/Solicitation.js
@@ -2,8 +2,14 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const dateformat = use('dateformat');
 
 class Solicitation extends Model {
+	//Gettings and Settings
+	 getCreated_at(created_at){
+	   return dateformat(created_at, "dd/mm/yyyy");
+	 }
+
     //Relacoes
     equipment(){
         return this.belongsTo('App/Models/Equipment');
